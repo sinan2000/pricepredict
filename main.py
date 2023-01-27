676e94954fa9b108db6ebb155cc2a0707a174037
sinan2000 <sinan_ceviker@yahoo.com>
2023-01-27 19:06:01 +0200

    Added volume and moving average as features for training model

--- main.py
+++ main.py
@@ -8,59 +8,63 @@ from sklearn.preprocessing import MinMaxScaler
 
 from info import api_key
 
-def predict(symbol):
-    # Collect data
-    #symbol = input('Stock Ticker: ')
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&outputsize=full&apikey={api_key}"
-    response = requests.get(url)
-    data = response.json()
-    stock_data = data['Time Series (Daily)']
-    df = pd.DataFrame(stock_data)
-    df = df.transpose()
-    df = df[['5. adjusted close']]
-    df.rename(columns={'5. adjusted close':'closing_price'}, inplace=True)
-    df.reset_index(drop=True, inplace=True)
-    df = df.iloc[::-1]
+# Retrieve data
+symbol = input('Stock Ticker: ')
+url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&outputsize=full&apikey={api_key}"
+response = requests.get(url)
+data = response.json()
+stock_data = data['Time Series (Daily)']
 
-    # Create training and test sets
-    scaler = MinMaxScaler()
-    scaled_data = scaler.fit_transform(df[['closing_price']])
-    time_steps = 30
-    num_features = 1
-    X_train = []
-    y_train = []
-    for i in range(time_steps, len(scaled_data)):
-        X_train.append(scaled_data[i-time_steps:i, 0])
-        y_train.append(scaled_data[i, 0])
-    X_train, y_train = np.array(X_train), np.array(y_train)
-    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], num_features))
+#Collect data
+df = pd.DataFrame(stock_data)
+df = df.transpose()
+df = df[['5. adjusted close', '6. volume']]
+df.rename(columns={'5. adjusted close':'closing_price', '6. volume':'volume'}, inplace=True)
+df.reset_index(drop=True, inplace=True)
+df = df.iloc[::-1]
 
-    # Create the LSTM model
-    model = Sequential()
-    model.add(LSTM(150, input_shape=(X_train.shape[1], num_features), return_sequences=False))
-    model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(X_train, y_train, epochs=30, batch_size=8)
+# Add moving average of 30 days to DataFrame
+df['mva_30'] = df['closing_price'].rolling(window=30).mean()
 
-    # Predict the future closing price in the next 30 days
-    future_prices = []
-    current_price = df.iloc[-time_steps:]['closing_price']
-    current_price = scaler.transform(current_price.values.reshape(-1, 1))
-    current_price = np.reshape(current_price, (1, time_steps, num_features))
-    for i in range(30):
-        next_price = model.predict(current_price)
-        future_prices.append(scaler.inverse_transform(next_price)[0][0])
-        current_price = np.append(current_price[:,1:,:], [[[next_price[0][0]]]], axis=1)
+# Create training and test sets
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(df[['closing_price', 'volume', 'mva_30']])
+time_steps = 30
+num_features = 3
+X_train = []
+y_train = []
+for i in range(time_steps, len(scaled_data)):
+    X_train.append(scaled_data[i-time_steps:i, :])
+    y_train.append(scaled_data[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], num_features))
 
-    print(future_prices)
+# Create the LSTM model
+model = Sequential()
+model.add(LSTM(150, input_shape=(X_train.shape[1], num_features), return_sequences=False))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(X_train, y_train, epochs=1, batch_size=8)
 
-    # Plot the prices
-    plt.plot(range(len(df)), df['closing_price'], color='blue', label='Historical Prices')
-    future_prices_index = range(len(df),len(df)+30)
-    future_prices = scaler.inverse_transform(np.array(future_prices).reshape(-1,1))
-    plt.plot(future_prices_index, future_prices, color='red', label='Predicted Prices')
-    plt.xlabel('Index')
-    plt.ylabel('Price')
-    plt.title('Historical and Predicted Prices')
-    plt.legend()
-    plt.show()
+# Predict the future closing price in the next 30 days
+future_prices = []
+current_price = df.iloc[-time_steps:][['closing_price', 'volume', 'mva_30']]
+current_price = scaler.transform(current_price)
+current_price = np.reshape(current_price, (1, time_steps, num_features))
+for i in range(30):
+    next_price = scaler.inverse_transform(next_price)
+    future_prices.append(scaler.inverse_transform(next_price)[0][0][0])
+    current_price = np.append(current_price[:,1:,:], [[next_price[0][0], current_price[0][-1][1], current_price[0][-1][2]]], axis=1)
+
+future_prices = np.array(future_prices).reshape(-1,1)
+print(future_prices)
+
+# Plot
+last_60_days = df.iloc[-60:]['closing_price']
+future_prices = scaler.inverse_transform(future_prices.reshape(-1,1))
+plt.plot(last_60_days, label='Last 60 Days', color='blue')
+plt.plot(range(60, 90), future_prices, label='Future Prices', color='red')
+plt.xlabel('Days')
+plt.ylabel('Closing Price')
+plt.legend()
+plt.show()
